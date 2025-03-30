851df21a69cce7662f92f43206a9482e92800af0
leoisole <leoisole@gmail.com>
2025-03-30 19:18:42 +0530

    new commit

--- src/gitpractice.js
+++ src/gitpractice.js
@@ -2,3 +2,5 @@ console.log('practicing git code')
 console.log(' this is how you learn stuff.')
 console.log('good job version 3');
 console.log('making new changes');
+
+console.log('commit after push');
